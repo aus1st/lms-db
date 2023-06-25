cb14c2a59fcb0ab36ba380820028c5a900751fbe
aus1st <aus1st@gmail.com>
2023-06-25 12:22:16 +0500

    url changed

--- src/app/db/index.js
+++ src/app/db/index.js
@@ -9,8 +9,8 @@ const { Pool } = pkg;
 const env = dotenv.config();
 
 const pool = new Pool({
-   // connectionString: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:5432/${env.POSTGRES_DATABASE}?sslmode=require`
-    connectionString: 'postgres://default:4UzYjlxt6HFR@ep-old-river-424254-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require'
+   connectionString: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:5432/${env.POSTGRES_DATABASE}?sslmode=require`
+   
 });
 
 
