aa015bf7247a27c16a04445bb32900d1542d3e74
Ahmed Uddin Siddiqui <aus1st@gmail.com>
2023-07-07 11:15:59 +0500

    cities working

--- src/app/api/city/route.ts
+++ src/app/api/city/route.ts
@@ -1,32 +1,33 @@
-import {db} from '@/app/db/drizzle'
+import { db } from '@/app/db/drizzle'
 import { centers, cities } from '@/app/db/schema/script';
 import { city } from '@/app/models/city';
 import { NextRequest, NextResponse } from 'next/server';
 
-export  async function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const req = await request.json();
-  const {cityName, campuses}: city = req;
-    try {
-        console.log('sending request to db')
-      const resCity = await db.insert(cities).values({
-        cityName:cityName,
-        
-      }).returning({cityId: cities.cityId});
+  const { cityName, campuses }: city = req;
+  try {
+    console.log('sending request to db', cityName, campuses)
 
-      // campuses?.map(async cent=>(
-      //  await db.insert(centers).values({
-      //   cityId: resCity[0].cityId,
-      //     campus: cent.campus,
-      //     centerName: cent.centerName,
-      //     location: cent.location  
-      //   }
-      // ))
-      // )
-    
-      console.log('response:',resCity)
-      return NextResponse.json({ resCity });
+    const resCity = await db.insert(cities).values({
+      cityName: cityName,
 
-    } catch (error) {
-      return NextResponse.json({ Error: error });
-    }
+    }).returning({ cityId: cities.cityId });
+
+    campuses?.map(async cent => (
+      await db.insert(centers).values({
+        cityId: resCity[0].cityId,
+        campus: cent.campus,
+        centerName: cent.centerName,
+        location: cent.location
+      }
+      ))
+    )
+
+    console.log('response:', resCity)
+    return NextResponse.json({ resCity });
+
+  } catch (error) {
+    return NextResponse.json({ Error: error });
+  }
 }
